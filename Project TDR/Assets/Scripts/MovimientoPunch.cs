0f619eaae368816641076c24d05a90aca165f26b
=Hector Garzon <59198132+Hecti1008@users.noreply.github.com>
2021-05-07 00:00:00 +0200

    Juntar projectes

--- Project TDR/Assets/Scripts/MovimientoPunch.cs
+++ Project TDR/Assets/Scripts/MovimientoPunch.cs
@@ -17,22 +17,23 @@ public class MovimientoPunch : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
             rb2D.velocity = new Vector2(runspeed, rb2D.velocity.y);
         }
         else if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            rb2D.velocity = new Vector2(runspeed, rb2D.velocity.y);
-
+            rb2D.velocity = new Vector2(-runspeed, rb2D.velocity.y);
         }
         else
         {
             rb2D.velocity = new Vector2(0, rb2D.velocity.y);
         }
-
+        if (Input.GetKey(KeyCode.Space) && CheckGround.isGrounded)
+        {
+            rb2D.velocity = new Vector2(rb2D.velocity.x, jumpspeed);
+        }
     }
 }

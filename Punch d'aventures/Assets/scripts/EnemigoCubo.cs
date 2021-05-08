150dda53f3a07482977d420280d3d80c2ab776bb
Ayman-Kaddar <ayman.kaddar@insbaixcamp.cat>
2021-05-09 01:54:28 +0200

    Cos

---
+++ Punch d'aventures/Assets/scripts/EnemigoCubo.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoCubo : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Jugador"))
+        {
+            Debug.Log("Daño Jugador");
+            Destroy(collision.gameObject);
+        }
+    }
+}

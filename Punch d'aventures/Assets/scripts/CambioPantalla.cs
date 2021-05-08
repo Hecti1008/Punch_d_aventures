37da245ee99a049e5e3ea0c2f02e1dc49c2da5ee
Ayman-Kaddar <ayman.kaddar@insbaixcamp.cat>
2021-05-08 16:22:16 +0200

    com

---
+++ Punch d'aventures/Assets/scripts/CambioPantalla.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CambioPantalla : MonoBehaviour
+{
+    private string movipantalla;
+
+    public void Movipantalla(string pantalla)
+    {
+        movipantalla = pantalla;
+        SceneManager.LoadScene(pantalla);
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}

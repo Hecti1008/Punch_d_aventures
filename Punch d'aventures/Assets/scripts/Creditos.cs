21be404926238d90cd37f6968d31d0538e206378
Ayman-Kaddar <ayman.kaddar@insbaixcamp.cat>
2021-05-14 21:18:24 +0200

    dwas

--- Punch d'aventures/Assets/scripts/Creditos.cs
+++ Punch d'aventures/Assets/scripts/Creditos.cs
@@ -8,7 +8,7 @@ public class Creditos : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("WaitToEnd", 10);
+        Invoke("WaitToEnd");
     }
 
     // Update is called once per frame
